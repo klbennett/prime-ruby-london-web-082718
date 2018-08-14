5d83edb523347c273720bf312cd94b7e3e759fc3
Kay B <kaylouisebennett@gmail.com>
2018-08-14 11:23:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+  def prime?(number)
+    if number > 0
+      false
+    end
+      for diviser in 2...(number - 1)
+       if (number % diviser) == 0
+        return false
+       end
+      end
+     true
+   end
